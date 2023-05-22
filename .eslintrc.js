329bb4bc81d293fb21448f80755e7070d70ba418
彭泽 <234277894@qq.com>
2023-05-22 09:25:38 +0800

    feat(custom): 标准化大厂编程规范解决方案之ESLint + Git Hooks

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,6 +13,9 @@ module.exports = {
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    // 'quotes': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    // 'comma-dangle': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    // 'semi': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
   }
 }
